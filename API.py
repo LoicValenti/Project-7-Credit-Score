1ec4abb125d161880e78ca9a6f70423e0b67e564
LoicValenti <86472405+LoicValenti@users.noreply.github.com>
2023-02-14 17:18:23 +0100

    Update API.py

--- API.py
+++ API.py
@@ -3,7 +3,6 @@ import uvicorn
 from fastapi import FastAPI
 
 filepath_predict_probs = "Credit Application Predict Probabilities.csv"
-
 client_predictions = pd.read_csv(filepath_predict_probs)
 
 app = FastAPI()
@@ -11,11 +10,10 @@ app = FastAPI()
 
 @app.get("/prediction/{client_id}")
 async def root(client_id: int):
-    client_id = int(client_id)
     if client_id in client_predictions["SK_ID_CURR"].values:
-
         # Insert the api request here
         prediction = client_predictions.loc[client_predictions["SK_ID_CURR"] == client_id].iloc[-1, 1]
+
         if prediction > 0.5:  # decision threshold customisable for risk level
             output = "Client's application was refused with {}% risk of defaulting".format(round(prediction * 100))
         else:
