29166793022027cbaee25c7f582ac058c919bb0c
LoicValenti <86472405+LoicValenti@users.noreply.github.com>
2023-02-14 17:07:18 +0100

    Update API.py

--- API.py
+++ API.py
@@ -2,16 +2,6 @@ import pandas as pd
 import uvicorn
 from fastapi import FastAPI
 
-
-def rescaling(i, min_wanted, max_wanted, actual_min, actual_max):
-    """
-    Rescales the data from the given min and max to the given values
-    To be used in a list creation function
-
-    """
-    return (max_wanted - min_wanted) * (i - actual_max) / (actual_min - actual_max) + min_wanted
-
-
 filepath_predict_probs = "Credit Application Predict Probabilities.csv"
 
 client_predictions = pd.read_csv(filepath_predict_probs)
@@ -26,7 +16,7 @@ async def root(client_id: int):
 
         # Insert the api request here
         prediction = client_predictions.loc[client_predictions["SK_ID_CURR"] == client_id].iloc[-1, 1]
-        if prediction > 0.5000000:  # Legacy function needs to be updated to reduce memory usage
+        if prediction > 0.5:  # decision threshold customisable for risk level
             output = "Client's application was refused with {}% risk of defaulting".format(round(prediction * 100))
         else:
             output = "Client's application was accepted with {}% chance of servicing the debt".format(round(
