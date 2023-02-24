72e0f087ec771daa3cfbf687e8f12af89fbc4d8d
LoicValenti <86472405+LoicValenti@users.noreply.github.com>
2023-02-24 12:15:21 +0100

    Create API.py

---
+++ API.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import uvicorn
+from fastapi import FastAPI
+
+filepath_predict_probs = "Credit Application Predict Probabilities.csv"
+client_predictions = pd.read_csv(filepath_predict_probs)
+
+app = FastAPI()
+
+
+@app.get("/prediction/{client_id}")
+async def root(client_id: int):
+    if client_id in client_predictions["SK_ID_CURR"].values:
+        # Insert the api request here
+        prediction = client_predictions.loc[client_predictions["SK_ID_CURR"] == client_id].iloc[-1, 1]
+
+        if prediction > 0.5:  # decision threshold customisable for risk level
+            output = "Client's application was refused with {}% risk of defaulting".format(round(prediction * 100))
+        else:
+            output = "Client's application was accepted with {}% chance of servicing the debt".format(round(
+                (1 - prediction) * 100))
+    else:
+        output = "Enter a valid client number in the space above"
+
+    return {"message": output}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app)
